d7a4185a9fca5849def01e70fae5bf8205b84c08
ilyasa Azmi <theazmi99ilyasa@gmail.com>
2020-03-07 22:40:39 +0700

    Initial Commit

---
+++ ImageMachine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageMachine
+//
+//  Created by Ilyasa Azmi on 07/03/20.
+//  Copyright © 2020 Ilyasa Azmi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
